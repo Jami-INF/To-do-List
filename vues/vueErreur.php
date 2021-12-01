5c9428e054c8d2ce84fec344e1db2b5db4ff5d83
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2021-12-01 16:02:33 +0100

    Update vueErreur.php
    Ajout css + code vue erreur

--- vues/vueErreur.php
+++ vues/vueErreur.php
@@ -1,23 +1,94 @@
-<DOCTYPE HTML>
-    <html>
-        <head>
-            <meta charset="utf-8" />
-            <title>Erreur</title>
-        </head>
-        <body>
-            <h1>Erreur</h1>
-            <p>
-                <?php echo $erreur; ?>
-            </p>
-        </body>
-
-<?php
-    if(isset($dataVueErreur))
-    {
-        foreach($dataVueErreur as $value)
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> ToDoux Liste </title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../sidebar.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <p class="logo_name">ToDoux Liste</p>
+      <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <ul class="nav-list">
+      <li>
+        <a href="/../index.php">
+         <i class='bx bx-home-alt' ></i>
+         <p class="links_name">Accueil</p>
+        </a>
+         <p class="tooltip">Accueil</p>
+      </li>
+      <li>
+       <a href="#">
+         <i class='bx bx-user' ></i>
+         <p class="links_name">Compte utilisateur</p>
+       </a>
+       <p class="tooltip">Compte utilisateur</p>
+     </li>
+     <li>
+       <a href="#">
+        <i class='bx bxs-graduation' ></i>
+        <p class="links_name">Espace de travail</p>
+       </a>
+       <p class="tooltip">Espace de travail</p>
+     </li>
+     <li>
+       <a href="#">
+        <i class='bx bx-list-ul'></i>
+        <p class="links_name">List 1</p>
+       </a>
+       <p class="tooltip">List 1</p>
+     </li>
+     <li>
+      <a href="#">
+        <i class='bx bx-list-ul'></i>
+        <p class="links_name">List 2</p>
+      </a>
+      <p class="tooltip">List 2</p>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-list-ul'></i>
+        <p class="links_name">List 3</p>
+      </a>
+      <p class="tooltip">List 3</p>
+    </li>
+     <li>
+       <a href="newList.php">
+        <i class='bx bx-plus' ></i>
+        <p class="links_name">Ajouter liste</p>
+       </a>
+       <p class="tooltip">Ajouter liste</p>
+     </li>
+     <li>
+       <a href="#">
+        <i class='bx bx-cog' ></i>
+        <p class="links_name">Setting</p>
+       </a>
+       <p class="tooltip">Setting</p>
+     </li>
+    </ul>
+  </div>
+  <section class="home-section">
+    <div class="main">
+    <?php
+        if(isset($dataVueErreur))
         {
-            echo $value;
+            foreach($dataVueErreur as $value)
+            {
+                echo $value;
+            }
         }
-    }
 
-?>
+    ?>
+    </div>     
+
+  </section>
+  <script src="../script.js"></script>
+</body>
+</html>
+
