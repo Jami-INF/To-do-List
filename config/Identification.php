c7aeb21c5779bb23bc825ff0fdd2286dc80c77dc
ZIRTR0X <76250995+ZIRTR0X@users.noreply.github.com>
2022-01-06 18:10:30 +0100

    Déploiement du site sur heroku
    Changement des identifiant de la BDD

--- config/Identification.php
+++ config/Identification.php
@@ -2,10 +2,10 @@
     $rep = __DIR__ . '/../'; // Répertoire racine
 
     //Base de données
-    $username = 'root';
-    $password = '';
-    $dsn = 'localhost';
-    $dbname = 'todoux';
+    $username = 'bd6c6d2633001e';
+    $password = '04c5c25a';
+    $dsn = 'us-cdbr-east-05.cleardb.net';
+    $dbname = 'heroku_4c02da699bdfc7a';
 
     //Vues
     $vues['Connection'] = 'vueConnection.php';
